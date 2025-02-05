bc52f623d45d79d69ef5e06e2fddce6435a3d576
RenatoYnonan <renatoignite1122@gmail.com>
2025-02-05 17:14:15 -0500

    Nuevos Cambios 05

--- MagdalenHouse/MagdalenHouse/urls.py
+++ MagdalenHouse/MagdalenHouse/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('base.urls', 'base'), namespace='base')),
+    path('magdalen_intranet/', include(('magdalen_intranet.urls', 'magdalen_intranet'), namespace='magdalen_intranet')),
 ]
