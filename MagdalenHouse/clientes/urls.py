c1d54f264eacca0841574a1540ce02fd2ae09337
RenatoYnonan <renatoignite1122@gmail.com>
2025-02-11 14:28:45 -0500

    cambios 13

--- MagdalenHouse/clientes/urls.py
+++ MagdalenHouse/clientes/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', ClientesView.as_view(), name='index-clientes'),
+    path('nuevo-cliente', CreateClientes.as_view(), name='crear-cliente'),
+    path('edit-cliente/<int:pk>', UpdateClientes.as_view(), name='editar-cliente'),
 ]
--- MagdalenHouse/clientes/views.py
+++ MagdalenHouse/clientes/views.py
@@ -1,27 +1,29 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
+
 from django.views.generic import *
 from django.urls import reverse_lazy
 from .forms import ClientesForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-        
+from django.views.generic import ListView, CreateView, UpdateView
+
 # Create your views here.
 
-class ClientesView(LoginRequiredMixin, TemplateView):   
+class ClientesView(LoginRequiredMixin, ListView):
+    model = ModelsClientes
+    context_object_name = 'clientes'
     template_name = 'index-clientes.html'
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['clientes'] = ModelsClientes.objects.all()
-        context['form'] = ClientesForm()
-        return context
-    
-    def post(self, request, *args, **kwargs):
-        form = ClientesForm(self.request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('clientes:index-clientes')
-        else:
-            context = self.get_context_data()
-            context['form'] = form
-            return render(request, self.template_name, context)
+
+class CreateClientes(LoginRequiredMixin, CreateView):
+    model = ModelsClientes
+    form_class = ClientesForm
+    template_name = 'index-clientes-form.html'
+    success_url = reverse_lazy('clientes:index-clientes')
+
+
+class UpdateClientes(LoginRequiredMixin, UpdateView):
+    model = ModelsClientes
+    form_class = ClientesForm
+    template_name = 'index-clientes-form.html'
+    success_url = reverse_lazy('clientes:index-clientes')
