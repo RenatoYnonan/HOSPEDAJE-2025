beb96bdf1c4036b0a46363c66af19e6a31e86c1c
citamedsac <citamedsac@gmail.com>
2025-02-10 20:52:25 -0500

    cambios 11

--- MagdalenHouse/reservas/views.py
+++ MagdalenHouse/reservas/views.py
@@ -1,6 +1,6 @@
 from webbrowser import get
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, View
 from .models import Reserva
 from .forms import ReservaForm
 from clientes.forms import ClientesForm
@@ -11,21 +11,34 @@ from django.http import JsonResponse
 class CalendarioView(TemplateView):
     template_name = 'index-calendario.html'
 
-class ReservasView(TemplateView):
+class ReservasView(View):
     template_name = 'index-reservas.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] =  ReservaForm()
-        context['form_customer'] =  ClientesForm
-        return context
-    
+    def get(self, request, *args, **kwargs):
+        context = {
+            'form': ReservaForm(),
+            'form_customer': ClientesForm(),
+        }
+        return render(request, self.template_name, context)
+
     def post(self, request, *args, **kwargs):
-        form = ClientesForm(self.request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('reservas:new-reserva')
-        else:
-            context = self.get_context_data()
-            context['form_customer'] = form
-            return render(request, self.template_name, context)
+        form_type = request.POST.get("form_type")
+
+        if form_type == "reservation":
+            form = ReservaForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('reservas:new-reserva')
+
+        elif form_type == "customer":
+            form_customer = ClientesForm(request.POST)
+            if form_customer.is_valid():
+                form_customer.save()
+                return redirect('reservas:new-reserva')
+
+        context = {
+            'form': ReservaForm(),
+            'form_customer': ClientesForm(),
+        }
+        return render(request, self.template_name, context)
+    
