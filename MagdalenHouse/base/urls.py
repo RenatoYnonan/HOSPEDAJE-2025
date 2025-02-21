7f6cf2f5840894b90239e201bfd996444b02afd1
RenatoYnonan <renatoignite1122@gmail.com>
2025-02-21 18:09:06 -0500

    cambios 21

--- MagdalenHouse/base/urls.py
+++ MagdalenHouse/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HospedajeView.as_view(), name='hospedaje-magdalen')
+    path('', views.HospedajeView.as_view(), name='hospedaje-magdalen'),
+    path('detail-habitacion/<int:pk>/', views.DetailAlojamiento.as_view(), name='detail-habitacion'),
 ]
--- MagdalenHouse/base/views.py
+++ MagdalenHouse/base/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import *
+from alojamiento.models import *
 
 # Create your views here.
 class HospedajeView(TemplateView):
     template_name = 'index-hospedaje.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['habitaciones'] = Habitaciones.objects.all()
+        context['departamentos'] = Departamentos.objects.all()
+        return context
+
+class DetailAlojamiento(DetailView):
+    model = Departamentos
+    context_object_name = 'alojamiento'
+    template_name = 'index-detail-habitacion.html'
